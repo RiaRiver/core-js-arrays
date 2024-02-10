e331871dee92134754895edc09dabe48ae2d155c
Ria River <riria.river@gmail.com>
2024-02-10 19:34:44 +0300

    feat: implement getIdentityMatrix function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -417,8 +417,10 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (_, ind) =>
+    Array.from({ length: n }, (__, ind2) => (ind === ind2 ? 1 : 0))
+  );
 }
 
 /**
