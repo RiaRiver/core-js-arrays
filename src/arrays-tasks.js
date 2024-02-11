f880ad834ff21935a7f2089a10f9418b3e21d3e8
Ria River <riria.river@gmail.com>
2024-02-11 17:48:39 +0300

    feat: implement sortDigitNamesByNumericOrder function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -565,8 +565,24 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const sortByDigitNames = (a, b) => {
+    const DIGITS_NAMES = {
+      zero: 0,
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+      seven: 7,
+      eight: 8,
+      nine: 9,
+    };
+    return DIGITS_NAMES[a] - DIGITS_NAMES[b];
+  };
+
+  return arr.sort(sortByDigitNames);
 }
 
 /**
